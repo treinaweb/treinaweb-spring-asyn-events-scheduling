903573c11711eb21a038b868543c7a5460bece4b
Cleyson <cleysonph@gmail.com>
2024-01-04 03:27:50 -0300

    02:03 - Utilizando Async no Spring

---
+++ src/main/java/br/com/treinaweb/asynceventsscheduling/config/AsyncConfig.java
@@ -0,0 +1,32 @@
+package br.com.treinaweb.asynceventsscheduling.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAX_POOL_SIZE = 4;
+    private static final int QUEU_CAPACITY = 10;
+    private static final String THREAD_NAME_PREFIX = "AsyncExecutor-";
+
+    @Override
+    @Nullable
+    public Executor getAsyncExecutor() {
+        var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEU_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.initialize();
+        return executor;
+    }
+    
+}
--- src/main/java/br/com/treinaweb/asynceventsscheduling/core/services/MailService.java
+++ src/main/java/br/com/treinaweb/asynceventsscheduling/core/services/MailService.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class MailService {
 
     private static final Duration DELAY = Duration.ofSeconds(2);
 
+    @Async
     public void send(String to, String subject, String body) {
         try {
             Thread.sleep(DELAY);
